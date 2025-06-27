490f97d5b3ca77fcec7d27b4187bba134c01a207
Mochytk <116850132+Mochytk@users.noreply.github.com>
2025-06-27 16:07:23 -0400

    Update lab4_ferrera_rojas.py

--- lab4_ferrera_rojas.py
+++ lab4_ferrera_rojas.py
@@ -11,6 +11,28 @@ class Celula(th.Thread):
         self.ronda_infeccion = None
         self.lock = th.Lock()
 
+def logica_rondas(rondas, celulas):
+    while rondas:
+        t_in = time.time()
+        while time.time() - t_in <= 10:
+            pos_1 = rd.random(0, 511)
+            pos_2 = rd.random(0, 511)
+            celula_1 = celulas[pos_1]
+            celula_2 = celulas[pos_2]
+            if pos_1 != pos_2:
+                celula_1, celula_2 = enfrentamiento(celula_1, celula_2)
+                # escribir en el archivo rondas
+        rondas -= 1
+
+def enfrentamiento(celula_1, celula_2):
+    if celula_1.tipo == "alien" and celula_2.tipo == "humano":
+        if rd.random() > 0.3:
+            celula_2.tipo = "alien"
+    if celula_2.tipo == "alien" and celula_1.tipo == "humano":
+        if rd.random() > 0.3:
+            celula_1.tipo = "alien"
+    return celula_1, celula_2
+
 def main_bonus():
     a = 0
     print("main_bonus")
