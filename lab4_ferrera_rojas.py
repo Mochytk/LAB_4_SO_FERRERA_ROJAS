8a1e2c0a9030e4274a1ea6d75b3c333e44ee1340
Mochytk <116850132+Mochytk@users.noreply.github.com>
2025-06-27 17:05:33 -0400

    Update lab4_ferrera_rojas.py

--- lab4_ferrera_rojas.py
+++ lab4_ferrera_rojas.py
@@ -3,6 +3,8 @@ import random as rd
 import time
 import math
 
+from regex import T
+
 class Celula(th.Thread):
     def __init__(self, id, tipo):
         self.id = id
@@ -11,12 +13,38 @@ class Celula(th.Thread):
         self.ronda_infeccion = None
         self.lock = th.Lock()
 
+    def infectar(self, ronda):
+        with self.lock:
+            if self.tipo == 'humana':
+                self.tipo = 'alien'
+                self.infectada = True
+                self.ronda_infeccion = ronda
+    
+def crear_celulas_base(total_celulas = 512, aliens_iniciales = 16):
+    # Números del 0 al 511 (ids de cada celula)
+    tot_cel = list(range(total_celulas))
+    cel_ali = rd.sample(tot_cel, aliens_iniciales)
+
+    celulas = []
+
+    for id in tot_cel:
+
+        if id in cel_ali:
+            tipo = 'alien'
+        else:
+            tipo = 'humano'
+
+        celula = Celula(id, tipo)
+        celulas.append(celula)
+
+    return celulas
+
 def main_bonus():
-    a = 0
     print("main_bonus")
 
 def main_base():
-    a = 0
+    celulas = crear_celulas_base()
+    
     print("main_base")
 
 bonus = False
